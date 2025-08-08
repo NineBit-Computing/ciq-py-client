f93b36b89ca020559ab266688caf2f6221b98d2c
Rajeev J <rajeev.jamaiyar@ninebit.in>
2025-08-08 16:29:19 +0530

    fix: enable sanity tests

--- examples/core-sanity/rag.py
+++ examples/core-sanity/rag.py
@@ -37,38 +37,34 @@ def handle_query_execution(query: str, callback=None):
     return response
 
 
-def main():
-    def on_done(error, data):
-        if error:
-            print(f"Error: Ingest_file failed: {error}")
-            sys.exit(1)
-        else:
-            print(f"Ingest_file succeeded: {str(data)}")
-
-    try:
-        if ENABLE_FILE_INGESTION is True:
-            handle_file_ingestion(file="examples/core-sanity/geo_chap_9.pdf", callback=on_done)
-
-        # if ENABLE_SHAREPOINT_TEST is True:
-        #     sp_config = get_sharepoint_configuration(
-        #         site_name="KnowledgeCenter", folder="CIQDocs", file_name="BOFA-CC-Elite.pdf"
-        #     )
-        #     remote_file_buffer = download_file_from_sharepoint(sp_config)
-        #     handle_file_ingestion(
-        #         file=io.BytesIO(remote_file_buffer),
-        #         callback=on_done,
-        #         associated_file_name=sp_config.get("target_file_name"),
-        #     )
-
-        if ENABLE_QUERY_TEST is True:
-            query = "What are land breeze?"
-            response = handle_query_execution(query=query)
-            print(f"Query response is {response}")
-
-    except Exception as ex:
-        print(f"Error while performing main execution: {ex}")
+def on_done(error, data):
+    if error:
+        print(f"Error: Ingest_file failed: {error}")
         sys.exit(1)
+    else:
+        print(f"Ingest_file succeeded: {str(data)}")
 
 
-# if __name__ == "__main__":
-# main()
+try:
+    if ENABLE_FILE_INGESTION is True:
+        handle_file_ingestion(file="examples/core-sanity/geo_chap_9.pdf", callback=on_done)
+
+    # if ENABLE_SHAREPOINT_TEST is True:
+    #     sp_config = get_sharepoint_configuration(
+    #         site_name="KnowledgeCenter", folder="CIQDocs", file_name="BOFA-CC-Elite.pdf"
+    #     )
+    #     remote_file_buffer = download_file_from_sharepoint(sp_config)
+    #     handle_file_ingestion(
+    #         file=io.BytesIO(remote_file_buffer),
+    #         callback=on_done,
+    #         associated_file_name=sp_config.get("target_file_name"),
+    #     )
+
+    if ENABLE_QUERY_TEST is True:
+        query = "What are land breeze?"
+        response = handle_query_execution(query=query)
+        print(f"Query response is {response}")
+
+except Exception as ex:
+    print(f"Error while performing main execution: {ex}")
+    sys.exit(1)
